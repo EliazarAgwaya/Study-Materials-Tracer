de7a3bb7140476ab78d2be96b694ceef95f88f2c
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-09-07 10:22:05 +0300

    Creates a header and an introductory paragraph to the home page

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,10 @@ import React from "react"
 function Home (){
     return(
         <div>
-            <h1>My home</h1>
+            <h1>Study Material Tracer</h1>
+            <p>Welcome to the study material tracer app! There is ability to add the study materials you've used, or
+          would like to. You can add reviews and rate books based on how
+          you liked them!</p>
         </div>
     )
 }
