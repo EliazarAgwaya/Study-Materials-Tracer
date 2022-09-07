b314d4e43201ce65e2d717391b255b514bb6d88b
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-09-07 15:40:39 +0300

    Creates the form field for adding a new material

--- src/Form.js
+++ src/Form.js
@@ -2,7 +2,26 @@ import React from "react"
  function Form (){
     return(
         <div>
+            <form>
+                <h4>Add a new material here: </h4>
 
+                <input
+                type="text"
+                placeholder="Material name"
+                name="name"
+                value="Material"
+                ></input>
+                <br />
+                <input
+                type="text"
+                placeholder="Enter Source"
+                name="Source"
+                value="Source"
+                ></input>
+                <br />
+
+                <input type="submit" />
+            </form>
         </div>
     )
  }
