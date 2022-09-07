c0d9422928fdfc36df6368fc586573a4daca97fc
EliazarAgwaya <eliazar.magero@student.moringaschool.com>
2022-09-07 12:00:19 +0300

    Gives routes to the NavBar links

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React from "react"
 import './App.css';
 import Materials from './Materials';
 import Form from './Form';
 import NavBar from './NavBar';
 import Home from './Home';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Home/>
-      <h2>I am trying</h2>
-      <Materials/>
-      <Form/>
+      <Routes>
+        <Route exact path="/materials" element={<Materials/>} />
+        <Route exact path="/form" element={<Form/>} />
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
     </div>
   );
 }
